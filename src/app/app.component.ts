3756128f07d4f5013cf32955f839cf22aedea20f
Daniel Forson <danielkojoforson27@gmail.com>
2023-12-27 17:45:29 +0100

    initial setup

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,14 +1,20 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
+import { FormControl, FormGroup, ReactiveFormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet],
+  imports: [CommonModule, RouterOutlet,ReactiveFormsModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
   title = 'entertainmentApp';
+  applyForm = new FormGroup({
+    firstName: new FormControl(""),
+    email: new FormControl(""),
+    password: new FormControl("")
+  });
 }
