e490dc53cb43d8e7596f6c5c06193c20cec7c6b4
Daniel Forson <danielkojoforson27@gmail.com>
2024-01-04 14:44:29 +0100

    added the dashboard to the routes

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,10 +2,12 @@ import { Routes } from '@angular/router';
 import { HomeComponent } from './home/home.component';
 import { UploadPhotoPageComponent } from './upload-photo-page/upload-photo-page.component';
 import { ContinuePageComponent } from './continue-page/continue-page.component';
+import { DashboardComponent } from './dashboard/dashboard.component';
 
 export const routes: Routes = [
   { path: '', component: HomeComponent },
   { path: 'app-upload', component: UploadPhotoPageComponent },
-  { path:'continue-page', component: ContinuePageComponent}
+  { path: 'continue-page', component: ContinuePageComponent },
+  {path: 'app-dashboard', component: DashboardComponent}
 
 ];
