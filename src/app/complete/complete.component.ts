7105cdccb2dc6704e567d4c23167a4f759b77514
Daniel Forson <danielkojoforson27@gmail.com>
2024-01-04 16:23:42 +0100

    addaed validation to the form

--- src/app/complete/complete.component.ts
+++ src/app/complete/complete.component.ts
@@ -1,12 +1,20 @@
 import { Component } from '@angular/core';
+import { Router, RouterLink, RouterLinkActive } from '@angular/router';
 
 @Component({
   selector: 'app-complete',
   standalone: true,
-  imports: [],
+  imports: [RouterLink, RouterLinkActive],
   templateUrl: './complete.component.html',
   styleUrl: './complete.component.css'
 })
 export class CompleteComponent {
+
+  constructor(public router: Router) { }
+  onInit(): void { }
+
+  onButtonClick() {
+    this.router.navigate(['']);
+  }
 
 }
--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,7 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterLink, RouterOutlet ,Router, RouterLinkActive} from '@angular/router';
-import { FormControl, FormGroup, ReactiveFormsModule } from '@angular/forms';
+import { FormControl, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
 
 
 @Component({
@@ -12,19 +12,21 @@ import { FormControl, FormGroup, ReactiveFormsModule } from '@angular/forms';
   styleUrl: './home.component.css'
 })
 export class HomeComponent implements OnInit {
-  applyForm = new FormGroup({
-    firstName: new FormControl(''),
-    email: new FormControl(''),
-    password: new FormControl(''),
+  // applyForm = new FormGroup({
+  //   firstName: new FormControl(''),
+  //   email: new FormControl(''),
+  //   password: new FormControl(''),
+  // });
+  registrationForm = new FormGroup({
+    name: new FormControl('', Validators.required),
+    email: new FormControl('', Validators.required),
+    password: new FormControl('', Validators.required)
   });
+  emoji: string = '../../assets/insert_emoji.png'
 
-  private _emoji: string = '../../assets/insert_emoji.png';
-  public get emoji(): string {
-    return this._emoji;
-  }
-  public set emoji(value: string) {
-    this._emoji = value;
-  }
+
+
+
 
   constructor(private router: Router) { }
 
