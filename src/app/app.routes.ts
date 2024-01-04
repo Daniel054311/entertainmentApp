a0e087ecb1e66aef2d4faf978afb1c2be49a4727
Daniel Forson <danielkojoforson27@gmail.com>
2024-01-04 14:59:44 +0100

    added complete component to the routes

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,11 +3,13 @@ import { HomeComponent } from './home/home.component';
 import { UploadPhotoPageComponent } from './upload-photo-page/upload-photo-page.component';
 import { ContinuePageComponent } from './continue-page/continue-page.component';
 import { DashboardComponent } from './dashboard/dashboard.component';
+import { CompleteComponent } from './complete/complete.component';
 
 export const routes: Routes = [
   { path: '', component: HomeComponent },
   { path: 'app-upload', component: UploadPhotoPageComponent },
   { path: 'continue-page', component: ContinuePageComponent },
-  {path: 'app-dashboard', component: DashboardComponent}
+  { path: 'app-dashboard', component: DashboardComponent },
+  { path: 'app-complete', component: CompleteComponent}
 
 ];
