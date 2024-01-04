a10f42b0d9cc624b348b5726263917116b62a913
Daniel Forson <danielkojoforson27@gmail.com>
2024-01-04 14:59:08 +0100

    generated the complete component

---
+++ src/app/complete/complete.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-complete',
+  standalone: true,
+  imports: [],
+  templateUrl: './complete.component.html',
+  styleUrl: './complete.component.css'
+})
+export class CompleteComponent {
+
+}
